af6275641d8721da93b0a18e9c16c5b7b08d8915
ekin-ceylan <43453604+ekin-ceylan@users.noreply.github.com>
2018-09-21 21:18:31 +0300

    Add to git

---
+++ sevenSegment.js
@@ -0,0 +1,103 @@
+﻿function panelEkle(ata, adet, pid) {
+
+    var panel = $("<div></div>");
+    panel.addClass("panel");
+    panel.appendTo(ata);
+    panel.attr("id", pid)
+
+    for (var i = 1; i <= adet; i++) {
+        var digit = $("<div></div>");
+        digit.addClass("digit");
+        digit.appendTo(panel);
+
+        for (var j = 1; j <= 7; j++) {
+            var segment = $("<div></div>");
+            segment.appendTo(digit);
+            var dvTop = $("<div></div>");
+            var dvBottom = $("<div></div>");
+            var golgeHor = $("<div></div>");
+
+            if (j == 1 || j == 4 || j == 7) {
+                segment.addClass("segmentHor")
+                dvTop.addClass("dvTop");
+                dvBottom.addClass("dvBottom");
+                golgeHor.addClass("golgeHor");
+            }
+            else {
+                segment.addClass("segmentVer")
+                dvTop.addClass("dvLeft");
+                dvBottom.addClass("dvRight");
+                golgeHor.addClass("golgeVer");
+            }
+            segment.append(dvTop, dvBottom, golgeHor);
+        }
+    }
+}
+
+function load(w, a) {
+    var seg;
+    var h = w / a;
+    var l = w - 9 * h;
+    var H = 2 * l + 12 * h;
+    var g = Math.round(0.5 * h);
+
+    $(".digit").css({ "width": w + "px", "height": H + "px" })
+    $(".segmentHor").css({ "width": w + "px", "height": 6 * h + "px" });
+    $(".segmentVer").css({ "width": 6 * h + "px", "height": H / 2 + 3 * h + "px" })
+    $(".dvLeft").css({ "border-top-width": h + "px", "border-bottom-width": h + "px", "border-left-width": h + "px", "height": H / 2 - 6 * h + "px" })
+    $(".dvRight").css({ "border-top-width": h + "px", "border-bottom-width": h + "px", "border-right-width": h + "px", "height": H / 2 - 6 * h + "px" })
+    $(".dvTop").css({ "border-top-width": h + "px", "border-left-width": h + "px", "border-right-width": h + "px", "width": w - 9 * h + "px" })
+    $(".dvBottom").css({ "border-bottom-width": h + "px", "border-left-width": h + "px", "border-right-width": h + "px", "width": w - 9 * h + "px" })
+    $(".golgeVer").css({ "border-radius": h + "px", "box-shadow": "red 0px 0px " + 5 * g + "px " + g + "px", "height": l + h + "px", "width": 1.6 * h + "px" })
+    $(".golgeHor").css({ "border-radius": h + "px", "box-shadow": "red 0px 0px " + 5 * g + "px " + g + "px", "width": l + h + "px", "height": 1.6 * h + "px" })
+
+}
+
+function yaz(sayi, alan) {
+    var b;
+    var d;
+    var dgt = document.getElementById(alan);
+
+    for (var i = dgt.children.length - 1 ; i >= 0; i--) {
+        b = sayi % 10;
+        d = dgt.children[i];
+        digitYaz(d, b, "red", "#700000");
+        sayi = (sayi - b) / 10;
+    }
+}
+
+function digitYaz(digit, i, l, d) {
+
+    if (!(i >= 0 && i < 10)) {
+        return;
+    }
+    var A = [l, d, l, l, d, l, l, l, l, l];
+    var B = [l, l, l, l, l, d, d, l, l, l];
+    var C = [l, l, d, l, l, l, l, l, l, l];
+    var D = [l, d, l, l, d, l, l, d, l, l];
+    var E = [l, d, l, d, d, d, l, d, l, d];
+    var F = [l, d, d, d, l, l, l, d, l, l];
+    var G = [d, d, l, l, l, l, l, d, l, l];
+
+    digit.children[0].children[0].style.borderTopColor = A[i];
+    digit.children[0].children[1].style.borderBottomColor = A[i];
+    digit.children[0].children[2].style.display = A[i] == l ? "initial" : "none";
+    digit.children[1].children[0].style.borderLeftColor = B[i];
+    digit.children[1].children[1].style.borderRightColor = B[i];
+    digit.children[1].children[2].style.display = B[i] == l ? "initial" : "none";
+    digit.children[2].children[0].style.borderLeftColor = C[i];
+    digit.children[2].children[1].style.borderRightColor = C[i];
+    digit.children[2].children[2].style.display = C[i] == l ? "initial" : "none";
+    digit.children[3].children[0].style.borderTopColor = D[i];
+    digit.children[3].children[1].style.borderBottomColor = D[i];
+    digit.children[3].children[2].style.display = D[i] == l ? "initial" : "none";
+    digit.children[4].children[0].style.borderLeftColor = E[i];
+    digit.children[4].children[1].style.borderRightColor = E[i];
+    digit.children[4].children[2].style.display = E[i] == l ? "initial" : "none";
+    digit.children[5].children[0].style.borderLeftColor = F[i];
+    digit.children[5].children[1].style.borderRightColor = F[i];
+    digit.children[5].children[2].style.display = F[i] == l ? "initial" : "none";
+    digit.children[6].children[0].style.borderTopColor = G[i];
+    digit.children[6].children[1].style.borderBottomColor = G[i];
+    digit.children[6].children[2].style.display = G[i] == l ? "initial" : "none";
+}
---
+++ slot.js
@@ -0,0 +1,186 @@
+﻿var timer = 0;
+var cizimAlani = [];
+var diskler = [];
+var a = 0;
+var resimler = ["kiraz.jpg", "portakal.jpg", "erik.jpg",
+                "limon.jpg", "bar.jpg", "7.jpg",
+                //"Slot/karpuz.jpg","Slot/muz.jpg", "Slot/win.jpg"
+];
+var prob = [
+    [3, 2, 8, 5, 6, 1],
+    [6, 9, 5, 1, 3, 1],
+    [7, 6, 2, 8, 1, 1]
+];
+var bhs, krd, win
+var sayac = -1;
+var tOut = 0;
+var adet = 25;
+
+function rulet(probs) {
+    var total = 0;
+    for (var i in probs) {
+        total += probs[i];
+    }
+
+    var r = Math.random() * total;
+    var i = 0;
+    for (; probs[i] < r; i++) {
+        probs[i + 1] = probs[i + 1] + probs[i];
+    }
+    return i
+}
+
+function hazirla() {
+    cizimAlani = [document.getElementById("tual").getContext("2d"),
+                  document.getElementById("tual2").getContext("2d"),
+                  document.getElementById("tual3").getContext("2d")];
+    for (var i = 0; i < 3; i++) {
+        diskler[i] = new disk();
+        diskler[i].olustur(adet, prob[i], i);
+    }
+    diskler[2].ikon[adet - 1].onload = ciz;
+}
+
+function disk() {
+    this.ikon = [];
+    this.hiz = 20;
+    this.y = 0;
+    this.j = 0;
+    this.yon = 1;
+
+    this.olustur = function (sayi, probs, z) {
+        for (var i = 0; i < sayi; i++) {
+            var img = document.createElement("img");
+            this.ikon[i] = img;
+            var a = rulet(probs.slice());
+            probs[a] = probs[a] - 1;
+            img.src = "Slot/" + resimler[a];
+        }
+    }
+}
+
+function baslat() {
+    if (timer) {
+        return;
+    }
+
+    krd -= bhs;
+    win = 0;
+    yaz(win, "Paid");
+    yaz(krd, "Krd");
+    for (var i = 0; i < 3; i++) {
+        diskler[i].hiz = 15;
+        diskler[i].yon = 1;
+        diskler[i].j = adet * 1000 + diskler[i].j % adet;
+    }
+    sayac = -1;
+    timer = setInterval(don, 10)
+}
+
+function don() {
+    if (!tOut) {
+        tOut = setTimeout(function () {
+            clearTimeout(tOut);
+            tOut = 0;
+            sayac++;
+        }, Math.round(Math.random() * 3000) + 100) ;
+    }
+    for (var i = 0; i < 3; i++) {
+        diskler[i].y = (diskler[i].y + diskler[i].hiz) % 120
+        if (diskler[i].y == 0) { diskler[i].j += diskler[i].yon; }
+        if (sayac == i && diskler[i].y == 0) { diskler[i].hiz = 0; diskler[i].yon = 0; }
+    }
+    ciz();
+    if (!(diskler[0].hiz + diskler[1].hiz + diskler[2].hiz)) {
+        clearInterval(timer);
+        clearTimeout(tOut);
+        tOut = 0;
+        hesapla()
+    }
+}
+
+function hesapla() {
+    var dsk1 = diskler[0].ikon[(diskler[0].j + 1) % adet].src.split("Slot/")[1];
+    var dsk2 = diskler[1].ikon[(diskler[1].j + 1) % adet].src.split("Slot/")[1];
+    var dsk3 = diskler[2].ikon[(diskler[2].j + 1) % adet].src.split("Slot/")[1];
+    
+
+    if (dsk1 == resimler[5] && dsk2 == resimler[5] && dsk3 == resimler[5] ) {
+        win = 1000;
+    }
+    else if (dsk1 == resimler[4] && dsk2 == resimler[4] && dsk3 == resimler[4]) {
+        win = 150;
+    }
+    else if (dsk1 == resimler[3] && dsk2 == resimler[3] && (dsk3 == resimler[3] || dsk3 == resimler[4])) {
+        win = 18;
+    }
+    else if (dsk1 == resimler[2] && dsk2 == resimler[2] && (dsk3 == resimler[2] || dsk3 == resimler[4])) {
+        win = 14;
+    }
+    else if (dsk1 == resimler[1] && dsk2 == resimler[1] && (dsk3 == resimler[1] || dsk3 == resimler[4])) {
+        win = 10;
+    }
+    else if (dsk1 == resimler[0] && dsk2 == resimler[0] && dsk3 == resimler[0]) {
+        win = 10;
+    }
+    else if (dsk1 == resimler[0] && dsk2 == resimler[0] || dsk1 == resimler[0] && dsk3 == resimler[0] || dsk3 == resimler[0] && dsk2 == resimler[0]) {
+        win = 5;
+    }
+    else if (dsk1 == resimler[0]) {
+        win = 3;
+    }
+
+
+    krd += win * bhs;
+
+    yaz(win, "Paid");
+    yaz(krd, "Krd");
+
+    if (bhs > krd) {
+        bhs = krd;
+        yaz(bhs, "Bet");
+    }
+
+    timer = 0;
+}
+
+function ciz() {
+    for (var m = 0; m < 3; m++) {
+        var a = diskler[m];
+        for (var i = 0; i < 4; i++) {
+            cizimAlani[m].drawImage(a.ikon[(i + a.j) % adet], 0, 0, 133, 133, 0, - a.yon * a.y + i * 120, 120, 120);
+        }
+    }
+}
+
+
+function betMax() {
+    if (timer) {
+        return;
+    }
+    if (krd < 9) {
+        bhs = krd;
+    }
+    else {
+        bhs = 9;
+    }
+    yaz(bhs, "Bet");
+}
+
+function betOne() {
+    if (timer) {
+        return;
+    }
+    if (bhs < krd && bhs < 9) {
+        bhs++;
+        yaz(bhs, "Bet");
+    }
+}
+
+function reset() {
+    if (timer) {
+        return;
+    }
+    bhs = 0;
+    yaz(bhs, 'Bet');
+}
